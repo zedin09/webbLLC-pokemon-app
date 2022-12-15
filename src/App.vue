7ade71bf9bd076b073645f54e2dba8c40949e5cf
zedin09 <zgarzon@unal.edu.co>
2022-12-15 12:41:26 -0500

    Creación del proyecto

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div >
+      <h1>Hello world with vue js</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
